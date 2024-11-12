3d2bdd1d35d709c45f590f72467730df10f1aa15
Nhan Vo <niick7@gmail.com>
2024-11-12 16:31:48 -0600

    Adding Button component

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,7 @@
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+
+
 export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
@@ -5,11 +9,11 @@ export default function MealItem({ meal }) {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add To Cart</button>
+          <Button>Add To Cart</Button>
         </p>
       </article>
     </li>
